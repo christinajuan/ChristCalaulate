30a878073191a86388585f98b0d1a008dee26510
christinajuan <christjuam1104@gmail.com>
2016-11-30 16:50:43 +0800

    2016-11-30

--- app/src/main/java/com/example/user/christcalaulate/MainActivity.java
+++ app/src/main/java/com/example/user/christcalaulate/MainActivity.java
@@ -6,15 +6,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button B7;
-    Button[] btns = new Button[15];
+    Button[] btns = new Button[10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        B7 = (Button)findViewById(R.id.b7);
         btns[0] = (Button)findViewById(R.id.b0);
         btns[1] = (Button)findViewById(R.id.b1);
         btns[2] = (Button)findViewById(R.id.b2);
@@ -25,20 +22,16 @@ public class MainActivity extends AppCompatActivity {
         btns[7] = (Button)findViewById(R.id.b7);
         btns[8] = (Button)findViewById(R.id.b8);
         btns[9] = (Button)findViewById(R.id.b9);
-        btns[10] = (Button)findViewById(R.id.b10);
-        btns[11] = (Button)findViewById(R.id.b11);
-        btns[12] = (Button)findViewById(R.id.b12);
-        btns[13] = (Button)findViewById(R.id.b13);
-        btns[14] = (Button)findViewById(R.id.b14);
 
-        for (int i = 0; i < 15; i++) {
+
+        for (int i = 0; i < 10; i++) {
             btns[i].setOnClickListener(clickListener);}
     }
 
     View.OnClickListener clickListener = new View.OnClickListener() {
 
         public void onClick(View v) {
-            for(int i = 0;i<15;i++) {
+            for(int i = 0;i<10;i++) {
                 btns[i].setBackgroundColor(getResources().getColor(R.color.colorOriginal));
             }
             v.setBackgroundColor(getResources().getColor(R.color.colorPink));
---
+++ app/src/main/java/com/example/user/christcalaulate/MyButton.java
@@ -0,0 +1,14 @@
+package com.example.user.christcalaulate;
+
+import android.content.Context;
+import android.widget.Button;
+
+/**
+ * Created by user on 2016/11/30.
+ */
+
+public class MyButton extends Button {
+    public MyButton(Context context) {
+        super(context);
+    }
+}
